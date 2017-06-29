3dae07c8ee6684014a126a417edfba9ae10634e0
York <anyone.1984@gmail.com>
2017-06-29 22:20:23 +0800

    add custom exception

--- dubbo-demo-provider/src/main/java/com/noisyle/dubbodemo/service/DemoServiceImpl.java
+++ dubbo-demo-provider/src/main/java/com/noisyle/dubbodemo/service/DemoServiceImpl.java
@@ -1,16 +1,18 @@
 package com.noisyle.dubbodemo.service;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
 
 @Service("demoService")
 public class DemoServiceImpl implements DemoService {
-	Logger logger = Logger.getLogger(DemoServiceImpl.class);
+	final private static Logger logger = LoggerFactory.getLogger(DemoServiceImpl.class);
 
 	public String test(String name) {
-		logger.info("Request: " + name);
+		logger.info("Request: {}", name);
 		if(name.length()==0){
-			throw new RuntimeException("Please input a name.");
+			throw new DemoException("Please input a name.");
 		}
 		return "hello " + name + "!";
 	}
